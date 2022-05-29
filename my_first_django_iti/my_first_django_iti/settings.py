1d7fb7e111232bfece2463fcce50e7e60f0e4619
Radwa Mostafa <82545700+radwa-mostafa-hassan@users.noreply.github.com>
2022-05-29 19:04:15 +0200

    Update settings.py

--- my_first_django_iti/my_first_django_iti/settings.py
+++ my_first_django_iti/my_first_django_iti/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$eebauz8h7em9+k)9#b-=%ept6ccydcegpiqb-yo4)#)1(74#3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
